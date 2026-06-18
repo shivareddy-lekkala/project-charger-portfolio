23636aa273c0004a22d5a2014bc1b9f44375bd19
shivareddy-lekkala <sivasamba241@gmail.com>
2026-06-18 15:39:32 +0530

    Fixed layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export const metadata: Metadata = {
   description:
     "Data Is Not Just Numbers. It's A Story Waiting To Be Told.",
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,7 +28,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
